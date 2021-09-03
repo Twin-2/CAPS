61f0497fedb392195005fa23bc404419bbd66853
Twin-2 <davidwhit818@gmail.com>
2021-09-02 18:27:29 -0700

    streamlined namespaces

--- driver/driver.js
+++ driver/driver.js
@@ -7,15 +7,6 @@ let baseURL = 'http://localhost:3232'
 const hub = io.connect(baseURL)
 const driver = io.connect(`${baseURL}/driver`)
 
-
-
-
-
-
-
-//alert system when picks up item and becomes in-transit
-//allerts system when an item is delivered
-
 hub.on('pickup', (payload) => {
     setTimeout(() => {
         console.log(`DRIVER: Picked up order ${payload.orderId}`)
@@ -24,7 +15,7 @@ hub.on('pickup', (payload) => {
 
     setTimeout(() => {
         console.log(`DRIVER: Delivered order ${payload.orderId}`)
-        driver.emit('delivered', payload)
+        hub.emit('delivered', payload)
     }, 3000)
 })
 
--- hub/capsHub.js
+++ hub/capsHub.js
@@ -17,21 +17,11 @@ io.on('connection', socket => {
         io.emit(`order ready for ${payload.store}`, payload)
     })
     socket.on('in-transit', inTransit)
-    // io.on('pickup', (payload) => {
-    //     driver.emit('pickup', payload)
-    // })
-    // io.on(`delivered`, payload => {
-    //     console.log('made it here')
-    //     socket.emit(`delivered for ${payload.store}`, payload)
-    // })
-    // io.on('delivered for Chiron', (payload) => {
-    //     socket.emit('delivered', payload)
-    // })
-    // io.on('delivered for Paingins_by_Laura', (payload) => {
-    //     socket.emit('delivered', payload)
-    // })
+    socket.on('delivered', payload => {
+        delivered(payload);
+        io.emit(`delivered for ${payload.store}`, payload)
+    })
 });
-
 
 Paintings_by_Laura.on('connection', socket => {
     console.log('Lauras store', socket.id)
@@ -41,26 +31,13 @@ Paintings_by_Laura.on('connection', socket => {
     })
 })
 
-
 Chiron.on('connection', socket => {
     console.log("David's store", socket.id)
     socket.on('pickup ready', payload => {
-        console.log('line 39')
         pickUp(payload);
         io.emit('pickup', payload)
     })
-
 })
-
-driver.on('connection', socket => {
-    console.log('driver', socket.id)
-    socket.on('delivered', payload => {
-        delivered(payload);
-        io.emit(`delivered for ${payload.store}`, payload)
-    })
-})
-
-
 
 function pickUp(payload) {
     let timestamp = new Date();
@@ -72,8 +49,6 @@ function pickUp(payload) {
     })
 }
 
-// events.on('in-transit', inTransit)
-
 function inTransit(payload) {
     let timestamp = new Date();
     console.log(
@@ -83,10 +58,6 @@ function inTransit(payload) {
         payload: payload
     })
 }
-
-// events.on('delivered', delivered)
-// // console.log('pickup', payload)
-
 
 function delivered(payload) {
     let timestamp = new Date();
